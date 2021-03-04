a5cc4c6b99042d1033881373c31b9917ef285579
ozanisgor <ozanisgor11@gmail.com>
2021-03-04 22:25:03 +0300

    webpack.config.js fie created and start config set

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module exports = {
+  'mode': 'none',
+  'entry': './src/index.js',
+  'output': {
+    'path': __dirname + '/dist',
+    'filename': 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
+  'module': {
+    'rules': []
+  }
+}
