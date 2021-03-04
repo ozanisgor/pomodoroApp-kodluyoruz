c7e5f70428aeab3500bf2650e23c63e983f7961f
ozanisgor <ozanisgor11@gmail.com>
2021-03-04 23:09:06 +0300

    babel config set for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,19 @@ module exports = {
         'style-loader',
         'css-loader'
       ]
-    }]
+    },
+      {
+        'test': /\.js$/,
+        'exclude': /node_modules/,
+        'use': [{
+          'loader': 'babel-loader',
+          'options': {
+            'presets': [
+              '@babel/preset-env'
+            ]
+          }
+        }]
+      }
+    ]
   }
 }
